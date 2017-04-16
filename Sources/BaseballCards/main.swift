728f2c647aab9f72779155c27d35ba1436698346
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2017-04-16 16:41:28 -0500

    Added POST api/v1/card/:id

--- Sources/BaseballCards/main.swift
+++ Sources/BaseballCards/main.swift
@@ -30,9 +30,9 @@ router.put("api/v1/card") { //4
 
     guard
         case let .json(cardJson) = body else { //6
-        _ = response.send(status: .unsupportedMediaType)
-        next()
-        return
+            _ = response.send(status: .unsupportedMediaType)
+            next()
+            return
     }
 
     let card = BaseballCard(playerName: cardJson["playerName"].stringValue,
@@ -53,9 +53,9 @@ router.get("api/v1/card/:id") { //1
 
     guard
         let id = request.parameters["id"] else { //2
-        _ = response.send(status: .badRequest)
-        next()
-        return
+            _ = response.send(status: .badRequest)
+            next()
+            return
     }
 
     if
@@ -67,6 +67,46 @@ router.get("api/v1/card/:id") { //1
         _ = response.send(status: .notFound) //5
         next()
     }
+}
+
+router.post("api/v1/card/:id", middleware: BodyParser()) //1
+router.post("api/v1/card/:id") { //2
+    (request, response, next) in
+
+    guard
+        let contentType = request.headers["Content-Type"],
+        contentType == "application/json",
+        let body = request.body,
+        let id = request.parameters["id"] else {
+            _ = response.send(status: .badRequest)
+            next()
+            return
+    }
+
+    guard
+        case let .json(cardJson) = body else { //4
+            _ = response.send(status: .unsupportedMediaType)
+            next()
+            return
+    }
+
+    guard
+        let oldCardIndex = cards.index(where: {$0.id == id}) else { //5
+            _ = response.send(status: .notFound) //6
+            next()
+            return
+    }
+
+    let newCard = BaseballCard(playerName: cardJson["playerName"].stringValue,
+                               teamNames: cardJson["teamNames"].arrayValue.map({$0.stringValue}),
+                               year: cardJson["year"].intValue,
+                               cardNumber: cardJson["cardNumber"].stringValue,
+                               cardCompanyName: cardJson["cardCompanyName"].stringValue,
+                               id: id) //7
+
+    cards[oldCardIndex] = newCard //8
+    _ = response.send(status: .OK)
+    next()
 }
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
