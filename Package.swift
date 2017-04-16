27fe611d0036c74c8cc9ca06403254bdccf66ea3
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2017-04-16 15:23:42 -0500

    Hello, World

--- Package.swift
+++ Package.swift
@@ -3,5 +3,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "BaseballCards"
+    name: "BaseballCards",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7)
+    ]
 )
--- Sources/BaseballCards/main.swift
+++ Sources/BaseballCards/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import Kitura //1
+
+let router = Router() //2
+
+router.get("/") { //3
+    (request: RouterRequest,
+    response: RouterResponse,
+    next: @escaping () -> Void) in //4
+    
+    response.send("Hello, World!") //5
+    next() //6
+}
+
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run() //7
