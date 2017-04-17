9e4d13a0c105f225527b1446f337b4a66cd8847d
jake.vanorder@gmail.com <jake.vanorder@gmail.com>
2017-04-17 05:34:13 -0500

    Added persistence to GET api/v2/card/:id

--- Sources/BaseballCards/main.swift
+++ Sources/BaseballCards/main.swift
@@ -18,7 +18,7 @@ router.get("/") {
     (request: RouterRequest,
     response: RouterResponse,
     next: @escaping () -> Void) in
-    
+
     response.send("Hello, World!")
     next()
 }
@@ -27,7 +27,7 @@ router.get("/") {
 router.put("api/v2/card", middleware: BodyParser())
 router.put("api/v2/card") {
     (request, response, next) in
-    
+
     guard
         let contentType = request.headers["Content-Type"],
         contentType == "application/json",
@@ -36,22 +36,22 @@ router.put("api/v2/card") {
             next()
             return
     }
-    
+
     guard
         case var .json(cardJson) = body else {
             _ = response.send(status: .unsupportedMediaType)
             next()
             return
     }
-    
+
     cardJson["type"].stringValue = "BaseballCard" //1
-    
+
     database.create(cardJson, callback: { //2
         (optionalID: String?,
         optionalRevision: String?,
         optionalDocument: JSON?,
         optionalError: Error?) in
-        
+
         guard
             let id = optionalID,
             let revision = optionalRevision,
@@ -60,40 +60,45 @@ router.put("api/v2/card") {
                 next()
                 return
         }
-        
-        
+
+
         _ = response.send(id) //4
         next()
     })
 }
 
 //MARK: GET
-router.get("api/v1/card/:id") {
+router.get("api/v2/card/:id") {
     (request, response, next) in
-    
+
     guard
         let id = request.parameters["id"] else {
             _ = response.send(status: .badRequest)
             next()
             return
     }
-    
-    if
-        let card = cards.filter({$0.id == id}).first {
-        response.send(json: JSON(card.dictionaryRepresentation))
-        next()
-    }
-    else {
-        _ = response.send(status: .notFound)
-        next()
-    }
+
+    database.retrieve(id, callback: { //1
+        (optionalJsonDocument: JSON?, optionalError: Error?) in
+
+        if
+            let jsonDocument = optionalJsonDocument,
+            jsonDocument["error"].string == .none { //2
+            let cardJson = BaseballCard.json(from: jsonDocument, with: id) //3
+            _ = response.send(json: cardJson)
+        }
+        else {
+            _ = response.send(status: .notFound) //4
+            next()
+        }
+    })
 }
 
 //MARK: POST
 router.post("api/v1/card/:id", middleware: BodyParser())
 router.post("api/v1/card/:id") {
     (request, response, next) in
-    
+
     guard
         let contentType = request.headers["Content-Type"],
         contentType == "application/json",
@@ -103,28 +108,28 @@ router.post("api/v1/card/:id") {
             next()
             return
     }
-    
+
     guard
         case let .json(cardJson) = body else {
             _ = response.send(status: .unsupportedMediaType)
             next()
             return
     }
-    
+
     guard
         let oldCardIndex = cards.index(where: {$0.id == id}) else {
             _ = response.send(status: .notFound)
             next()
             return
     }
-    
+
     let newCard = BaseballCard(playerName: cardJson["playerName"].stringValue,
                                teamNames: cardJson["teamNames"].arrayValue.map({$0.stringValue}),
                                year: cardJson["year"].intValue,
                                cardNumber: cardJson["cardNumber"].stringValue,
                                cardCompanyName: cardJson["cardCompanyName"].stringValue,
                                id: id)
-    
+
     cards[oldCardIndex] = newCard
     _ = response.send(status: .OK)
     next()
@@ -133,21 +138,21 @@ router.post("api/v1/card/:id") {
 //MARK: DELETE
 router.delete("api/v1/card/:id") {
     (request, response, next) in
-    
+
     guard
         let id = request.parameters["id"] else {
             _ = response.send(status: .badRequest)
             next()
             return
     }
-    
+
     guard
         let oldCardIndex = cards.index(where: {$0.id == id}) else {
             _ = response.send(status: .notFound)
             next()
             return
     }
-    
+
     cards.remove(at: oldCardIndex)
     _ = response.send(status: .OK)
     next()
